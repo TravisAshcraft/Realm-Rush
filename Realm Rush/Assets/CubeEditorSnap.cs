002bbc9df066c6fd0dbff999467cd80f5146aeaa
TravisAshcraft <58603687+TravisAshcraft@users.noreply.github.com>
2020-01-31 12:36:17 -0600

    Added text mesh to top face of cubes allwoing them to track world pos

--- Realm Rush/Assets/CubeEditorSnap.cs
+++ Realm Rush/Assets/CubeEditorSnap.cs
@@ -3,16 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class EditorSnap : MonoBehaviour
+public class CubeEditorSnap : MonoBehaviour
 {
     [SerializeField][Range(1f,20f)]float gridSize = 10f;
-    private void Update()
+
+    TextMesh textMesh;
+
+    private void Start()
     {
         
+    }
+
+    private void Update()
+    {
         Vector3 snapPos;
+        
         snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
         snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
-
         transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+
+        textMesh = GetComponentInChildren<TextMesh>(); //this is how you find a component in a child;
+        textMesh.text = snapPos.x / gridSize + "," + snapPos.z / gridSize;
     }
 }
