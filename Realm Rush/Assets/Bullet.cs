2e8c3e12d98c44d43b16dfc8759087d38affcc77
TravisAshcraft <58603687+TravisAshcraft@users.noreply.github.com>
2020-02-08 14:59:43 -0600

    Added particle effect bullet added script to instaniate particle revised pathfinder to allow more than one enemy

---
+++ Realm Rush/Assets/Bullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    [SerializeField] GameObject bullet;
+    [SerializeField] Transform bulletSpawnPoint;
+    [SerializeField] GameObject enemy;
+     float bulletSpeed = 100f;
+    Rigidbody bulletRigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bulletRigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        
+        
+    }
+}
--- Realm Rush/Assets/Scripts/Tower.cs
+++ Realm Rush/Assets/Scripts/Tower.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,10 +7,41 @@ public class Tower : MonoBehaviour
 {
     [SerializeField] Transform objectToPan;
     [SerializeField] Transform targetEnemy;
+    [SerializeField] ParticleSystem projectileParticle;
+    float attackRange = 30f;
 
     // Update is called once per frame
     void Update()
     {
-        objectToPan.LookAt(targetEnemy);
+        if (!targetEnemy)
+        {
+            Shoot(false);
+            return;
+        }
+        else
+        {
+            objectToPan.LookAt(targetEnemy);
+            FireAtEnemy();
+        }
+       
+    }
+
+    private void FireAtEnemy()
+    {
+        float distanceToEnemy = Vector3.Distance(targetEnemy.transform.position, gameObject.transform.position); // creates the distance var between the enemy and the tower
+        if (distanceToEnemy <= attackRange)
+        {
+            Shoot(true);
+        }
+        else
+        {
+            Shoot(false);
+        }
+    }
+
+    private void Shoot(bool isActive)
+    {
+        var emissionModule = projectileParticle.emission;
+        emissionModule.enabled = isActive;
     }
 }
