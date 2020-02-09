9ca842ac19989f41cfa1edacdf9ca09d730b4917
TravisAshcraft <58603687+TravisAshcraft@users.noreply.github.com>
2020-02-08 18:16:58 -0600

    Added enemy spawn coroutine

---
+++ Realm Rush/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] GameObject enemy;
+    [SerializeField] Transform spawnPoint;
+    float timeBetweenSpawns = 4f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnEnemies());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnEnemies()
+    {
+        yield return new WaitForSeconds(timeBetweenSpawns);
+        GameObject enemyClone = Instantiate(enemy, spawnPoint.position, spawnPoint.rotation) as GameObject;
+    }
+}
